3b92f8d7e8cae7602f2711b853d2cbf8ee5e84f4
Sergey Ilin <SergeyIlinR@gmail.com>
2023-12-22 23:31:55 +0300

    improved

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,57 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    public static final String PLUS = "+";
+    public static final String SUBTRACTION = "-";
+    public static final String MULTIPLICATION = "*";
+    public static final String DIVISION = "/";
+
     public static void main(String[] args) throws Exception {
+
         Scanner scanner = new Scanner(System.in);
         String expression = scanner.nextLine();
-        String[] splitedExpression = expression.split(" ");
-        System.out.println(Arrays.toString(splitedExpression));
-        String operandFirst = splitedExpression[0];
-        String operandSecond = splitedExpression[2];
-        String operation = splitedExpression[1];
+        String[] splitedExpressionBySpace = expression.split(" ");
+        String operation = null;
+        for (String s : splitedExpressionBySpace) {
+            if (s.equals(PLUS) || s.equals(SUBTRACTION) || s.equals(MULTIPLICATION) || s.equals(DIVISION)) {
+                operation = s;
+            }
+        }
+        if (operation == null) {
+            throw new Exception();
+        }
+        String operandFirst = null;
+        String operandSecond = null;
         String result = "";
+        String regexOperation = null;
+        String[] splitedExpressionBySign = null;
         switch (operation) {
-            case "+":
+
+            case "+": {
+                regexOperation = operation.replaceAll("\\+", "\\\\+");
+                splitedExpressionBySign = expression.split(regexOperation);
+                operandFirst = splitedExpressionBySign[0].trim();
+                operandSecond = splitedExpressionBySign[1].trim();
                 operandFirst = operandFirst.substring(1, operandFirst.length() - 1);
                 operandSecond = operandSecond.substring(1, operandSecond.length() - 1);
                 result = operandFirst + operandSecond;
                 break;
+            }
             case "-": {
+//                regexOperation = operation.replaceAll("\\-", "\\\\-");
+                splitedExpressionBySign = expression.split(operation);
+                operandFirst = splitedExpressionBySign[0].trim();
+                operandSecond = splitedExpressionBySign[1].trim();
                 operandFirst = operandFirst.substring(1, operandFirst.length() - 1);
                 operandSecond = operandSecond.substring(1, operandSecond.length() - 1);
                 result = operandFirst.replace(operandSecond, "");
                 break;
             }
             case "*": {
+                regexOperation = operation.replaceAll("\\*", "\\\\*");
+                splitedExpressionBySign = expression.split(regexOperation);
+                operandFirst = splitedExpressionBySign[0].trim();
+                operandSecond = splitedExpressionBySign[1].trim();
                 operandFirst = operandFirst.substring(1, operandFirst.length() - 1);
                 int intValue = Integer.parseInt(operandSecond);
                 if (intValue <= 10 && intValue > 0) {
@@ -36,6 +64,10 @@ public class Main {
                 break;
             }
             case "/": {
+//                regexOperation = operation.replaceAll("\\/", "\\\\/");
+                splitedExpressionBySign = expression.split(operation);
+                operandFirst = splitedExpressionBySign[0].trim();
+                operandSecond = splitedExpressionBySign[1].trim();
                 operandFirst = operandFirst.substring(1, operandFirst.length() - 1);
                 int intValue = Integer.parseInt(operandSecond);
                 if (intValue <= 10 && intValue > 0) {
